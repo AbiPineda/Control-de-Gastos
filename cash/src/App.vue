520473ff1a484ef3cd52018eb40956b5740ec2a4
Abigail <pineverdi@gmail.com>
2022-07-27 11:05:04 -0600

    Splashscreen
    Pantalla Splashscreen (pantalla de inicio)
    la cual por medio de un suspense renderiza a la siguiente pantalla dependiendo del tiempo establecido

--- cash/src/App.vue
+++ cash/src/App.vue
@@ -1,15 +1,42 @@
 <template>
-  <HomeCashVue></HomeCashVue>
+  <Suspense>
+    <!-- #default: nombre del slot -->
+    <template #default>
+      <!-- al terminar de cargar el splashScreen mostrara el Home -->
+      <HomeCash />
+    </template>
+    <template #fallback>
+         <!-- se cargara al inicio -->
+        <SplashscreenCash/>
+    </template>
+  </Suspense>
 </template>
 
 <script>
-import HomeCashVue from "./components/HomeCash.vue";
+import SplashscreenCash from "./components/SplashscreenCash.vue";
+import { defineAsyncComponent } from "vue";
+import HomeCash from "./components/HomeCash.vue";
 
 export default {
   components: {
-    HomeCashVue,
-  },
+    SplashscreenCash,
+    HomeCash: defineAsyncComponent(() => new Promise((resolve) => setTimeout(() => {
+        resolve(import("./components/HomeCash.vue"));
+    }, 2500))),
+},
 };
 </script>
 
-<style></style>
+<style>
+html,
+body,
+.app {
+  min-height: 100vh;
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+* {
+  --brand-green: #04b500;
+  --brand-blue: #0689b0;
+}
+</style>
